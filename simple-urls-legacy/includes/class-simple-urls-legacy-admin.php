a74cba62c302c526320079889ede92c23158d9e2
Matt Ryan <matt@capwebsolutions.com>
2022-08-09 10:53:27 -0400

    Adjust source for current phpcs

--- simple-urls-legacy/includes/class-simple-urls-legacy-admin.php
+++ simple-urls-legacy/includes/class-simple-urls-legacy-admin.php
@@ -3,25 +3,24 @@
 /**
  * Simple_Urls_Admin file.
  *
- * @package simple-urls
+ * @package simple-urls-legacy
  */
 
 /**
  * Simple_Urls_Admin class.
  */
-class Simple_Urls_Legacy_Admin
-{
+class Simple_Urls_Legacy_Admin {
+
 
 	/**
 	 * Constructor.
 	 */
-	public function __construct()
-	{
-		add_filter('post_updated_messages', array($this, 'updated_message'));
-		add_action('admin_menu', array($this, 'add_meta_box'));
-		add_action('save_post', array($this, 'meta_box_save'), 1, 2);
-		add_action('manage_posts_custom_column', array($this, 'columns_data'));
-		add_filter('manage_edit-surleg_columns', array($this, 'columns_filter'));
+	public function __construct() {
+		add_filter( 'post_updated_messages', array( $this, 'updated_message' ) );
+		add_action( 'admin_menu', array( $this, 'add_meta_box' ) );
+		add_action( 'save_post', array( $this, 'meta_box_save' ), 1, 2 );
+		add_action( 'manage_posts_custom_column', array( $this, 'columns_data' ) );
+		add_filter( 'manage_edit-surleg_columns', array( $this, 'columns_filter' ) );
 	}
 
 	/**
@@ -31,15 +30,14 @@ class Simple_Urls_Legacy_Admin
 	 *
 	 * @return array          Filtered columns.
 	 */
-	public function columns_filter($columns)
-	{
+	public function columns_filter( $columns ) {
 
 		$columns = array(
 			'cb'        => '<input type="checkbox" />',
-			'title'     => __('Title', 'simple-urls-legacy'),
-			'url'       => __('Redirect to', 'simple-urls-legacy'),
-			'permalink' => __('Permalink', 'simple-urls-legacy'),
-			'clicks'    => __('Clicks', 'simple-urls-legacy'),
+			'title'     => __( 'Title', 'simple-urls-legacy' ),
+			'url'       => __( 'Redirect to', 'simple-urls-legacy' ),
+			'permalink' => __( 'Permalink', 'simple-urls-legacy' ),
+			'clicks'    => __( 'Clicks', 'simple-urls-legacy' ),
 		);
 
 		return $columns;
@@ -50,13 +48,12 @@ class Simple_Urls_Legacy_Admin
 	 *
 	 * @param  array $column Columns.
 	 */
-	public function columns_data($column)
-	{
+	public function columns_data( $column ) {
 
 		global $post;
 
-		$url   = get_post_meta($post->ID, '_surleg_redirect', true);
-		$count = get_post_meta($post->ID, '_surleg_count', true);
+		$url   = get_post_meta( $post->ID, '_surleg_redirect', true );
+		$count = get_post_meta( $post->ID, '_surleg_count', true );
 
 		$allowed_tags = array(
 			'a' => array(
@@ -65,12 +62,12 @@ class Simple_Urls_Legacy_Admin
 			),
 		);
 
-		if ('url' === $column) {
-			echo wp_kses(make_clickable(esc_url($url ? $url : '')), $allowed_tags);
-		} elseif ('permalink' === $column) {
-			echo wp_kses(make_clickable(get_permalink()), $allowed_tags);
-		} elseif ('clicks' === $column) {
-			echo esc_html($count ? $count : 0);
+		if ( 'url' === $column ) {
+			echo wp_kses( make_clickable( esc_url( $url ? $url : '' ) ), $allowed_tags );
+		} elseif ( 'permalink' === $column ) {
+			echo wp_kses( make_clickable( get_permalink() ), $allowed_tags );
+		} elseif ( 'clicks' === $column ) {
+			echo esc_html( $count ? $count : 0 );
 		}
 	}
 
@@ -81,18 +78,17 @@ class Simple_Urls_Legacy_Admin
 	 *
 	 * @return array           Messages.
 	 */
-	public function updated_message($messages)
-	{
+	public function updated_message( $messages ) {
 
-		$surl_object = get_post_type_object('surleg');
+		$surl_object = get_post_type_object( 'surleg' );
 
 		$messages['surleg'] = $surl_object->labels->messages;
 
 		$permalink = get_permalink();
 
-		if ($permalink) {
-			foreach ($messages['surleg'] as $id => $message) {
-				$messages['surleg'][$id] = sprintf($message, $permalink);
+		if ( $permalink ) {
+			foreach ( $messages['surleg'] as $id => $message ) {
+				$messages['surleg'][ $id ] = sprintf( $message, $permalink );
 			}
 		}
 
@@ -102,28 +98,26 @@ class Simple_Urls_Legacy_Admin
 	/**
 	 * Add metabox.
 	 */
-	public function add_meta_box()
-	{
-		add_meta_box('surleg', __('URL Information', 'simple-urls-legacy'), array($this, 'meta_box'), 'surleg', 'normal', 'high');
+	public function add_meta_box() {
+		add_meta_box( 'surleg', __( 'URL Information', 'simple-urls-legacy' ), array( $this, 'meta_box' ), 'surleg', 'normal', 'high' );
 	}
 
 	/**
 	 * Metabox.
 	 */
-	public function meta_box()
-	{
+	public function meta_box() {
 
 		global $post;
 
-		printf('<input type="hidden" name="_surleg_nonce" value="%s" />', esc_attr(wp_create_nonce(plugin_basename(__FILE__))));
+		printf( '<input type="hidden" name="_surleg_nonce" value="%s" />', esc_attr( wp_create_nonce( plugin_basename( __FILE__ ) ) ) );
 
-		printf('<p><label for="%s">%s</label></p>', '_surleg_redirect', esc_html__('Redirect URI', 'simple-urls-legacy'));
-		printf('<p><input style="%s" type="text" name="%s" id="%s" value="%s" /></p>', 'width: 99%;', '_surleg_redirect', '_surleg_redirect', esc_attr(get_post_meta($post->ID, '_surleg_redirect', true)));
-		printf('<p><span class="description">%s</span></p>', esc_html__('This is the URL that the Redirect Link you create on this page will redirect to when accessed in a web browser.', 'simple-urls-legacy'));
+		printf( '<p><label for="%s">%s</label></p>', '_surleg_redirect', esc_html__( 'Redirect URI', 'simple-urls-legacy' ) );
+		printf( '<p><input style="%s" type="text" name="%s" id="%s" value="%s" /></p>', 'width: 99%;', '_surleg_redirect', '_surleg_redirect', esc_attr( get_post_meta( $post->ID, '_surleg_redirect', true ) ) );
+		printf( '<p><span class="description">%s</span></p>', esc_html__( 'This is the URL that the Redirect Link you create on this page will redirect to when accessed in a web browser.', 'simple-urls-legacy' ) );
 
-		$count = isset($post->ID) ? get_post_meta($post->ID, '_surleg_count', true) : 0;
+		$count = isset( $post->ID ) ? get_post_meta( $post->ID, '_surleg_count', true ) : 0;
 		/* translators: %d is the counter of clicks. */
-		echo '<p>' . sprintf(esc_html__('This URL has been accessed %d times', 'simple-urls-legacy'), esc_attr($count)) . '</p>';
+		echo '<p>' . sprintf( esc_html__( 'This URL has been accessed %d times', 'simple-urls-legacy' ), esc_attr( $count ) ) . '</p>';
 	}
 
 	/**
@@ -132,44 +126,43 @@ class Simple_Urls_Legacy_Admin
 	 * @param  string  $post_id Post Id.
 	 * @param  WP_Post $post   Post.
 	 */
-	public function meta_box_save($post_id, $post)
-	{
+	public function meta_box_save( $post_id, $post ) {
 
 		$key = '_surleg_redirect';
 
 		// Verify the nonce.
-		// phpcs:ignore
-		if (!isset($_POST['_surleg_nonce']) || !wp_verify_nonce($_POST['_surleg_nonce'], plugin_basename(__FILE__))) {
+        // phpcs:ignore
+        if (!isset($_POST['_surleg_nonce']) || !wp_verify_nonce($_POST['_surleg_nonce'], plugin_basename(__FILE__))) {
 			return;
 		}
 
 		// Don't try to save the data under autosave, ajax, or future post.
-		if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 			return;
 		};
 
-		if (defined('DOING_AJAX') && DOING_AJAX) {
+		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
 			return;
 		};
 
-		if (defined('DOING_CRON') && DOING_CRON) {
+		if ( defined( 'DOING_CRON' ) && DOING_CRON ) {
 			return;
 		};
 
 		// Is the user allowed to edit the URL?
-		if (!current_user_can('edit_posts') || 'surleg' !== $post->post_type) {
+		if ( ! current_user_can( 'edit_posts' ) || 'surleg' !== $post->post_type ) {
 			return;
 		}
 
-		// phpcs:ignore
-		$value = isset($_POST[$key]) ? $_POST[$key] : '';
+        // phpcs:ignore
+        $value = isset($_POST[$key]) ? $_POST[$key] : '';
 
-		if ($value) {
+		if ( $value ) {
 			// Save/update.
-			update_post_meta($post->ID, $key, $value);
+			update_post_meta( $post->ID, $key, $value );
 		} else {
 			// Delete if blank.
-			delete_post_meta($post->ID, $key);
+			delete_post_meta( $post->ID, $key );
 		}
 	}
 }
--- simple-urls-legacy/plugin.php
+++ simple-urls-legacy/plugin.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Plugin Name: Simple URLs Legacy by Cap Web Solutions
+ * Plugin Name: Simple URLs Legacy
  * Plugin URI: https://github.com/capwebsolutions/simple-urls-legacy/
- * Description: Simple URLs Legacy is a complete URL management system that allows you create, manage, and track outbound links from your site by using custom post types and 301 redirects. The way it used to be.
+ * Description: Simple URLs Legacy is a fork of the orignial plugin by Nathan Rice and Studiopress team. As such, it is a complete URL management system that allows you create, manage, and track outbound links from your site by using custom post types and 301 redirects, as it was meant to be.
  * Author: Cap Web Solutions/Matt Ryan
  * Author URI: https://capwebsolutions.com/
- * Version: 1.0.0
+ * Version: 0.10.0
 
  * Text Domain: simple-urls-legacy
  * Domain Path: /languages
@@ -17,18 +17,18 @@
  * @package simple-urls-legacy
  */
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define('SIMPLE_URLS_LEGACY_DIR', plugin_dir_path(__FILE__));
-define('SIMPLE_URLS_LEGACY_URL', plugins_url('', __FILE__));
+define( 'SIMPLE_URLS_LEGACY_DIR', plugin_dir_path( __FILE__ ) );
+define( 'SIMPLE_URLS_LEGACY_URL', plugins_url( '', __FILE__ ) );
 
 require_once SIMPLE_URLS_LEGACY_DIR . '/includes/class-simple-urls-legacy.php';
 
 new Simple_Urls_Legacy();
 
-if (is_admin()) {
-	require_once SIMPLE_URLS_LEGACY_DIR . '/includes/class-simple-urls-legacy-admin.php';
+if ( is_admin() ) {
+	include_once SIMPLE_URLS_LEGACY_DIR . '/includes/class-simple-urls-legacy-admin.php';
 	new Simple_Urls_Legacy_Admin();
 }
